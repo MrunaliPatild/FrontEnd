895de810e26d311d1c4635dbd6af91a7cd7db205
MrunaliPatild <118677896+MrunaliPatild@users.noreply.github.com>
2023-04-26 13:16:48 +0530

    Add files via upload

---
+++ daythree/daythree.js
@@ -0,0 +1,32 @@
+/*alert("Congratulations!")
+alert("Hello")
+alert(4+2)
+let num=15;
+    alert(num)
+const c=15;
+    alert(c)
+var v="Bye"
+    alert(v)*/
+    /*let a=13
+    //assignment operator
+    a*=5
+    alert(a)
+    //relational operator
+    var res=12==0
+    alert(res)
+    //logical operator
+    let b=5<6&&15>9
+    alert(b)*/
+    var x=13;
+    var y=12;
+    alert(x==y && x>y)
+    if(x>y)
+    alert("Mrunali")
+    else
+    alert("Patil")
+    
+    alert(typeof(true))
+    
+    let age=prompt("what is your age?",100)
+        alert("your age is"+age)
+       
---
+++ daythree/intro.js
@@ -0,0 +1,6 @@
+function printLength(){
+    var str=document.getElementById("name").value;
+    var strlen=str.length;
+    const x=44;
+    document.getElementById("demo").innerHTML="your score is:"+strlen+x;
+}
